d8a3b4324e6d1dc6ab900bd9163242a2b9eea6db
ghostlers <119812061+ghostlers@users.noreply.github.com>
2022-12-08 17:08:40 +0000

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'Ghostlers',
   description: 'The Last test version of Ghostlers Contract',
-    contractAddress: '0xC44BDdcAb5e6587caA1991Faf2390b2D01d06604',
+    contractAddress: '0xc168abf1cadE312776e476E6A58704F39Fcb070c',
   maxMintAmount: 2,
   presaleMaxMintAmount: 4,
   price: 0.00
